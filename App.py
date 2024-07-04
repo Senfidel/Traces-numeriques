feee4242a09a25b5925dbfe1464397c8c764dc63
Senfidel <137909173+Senfidel@users.noreply.github.com>
2024-07-04 21:14:20 +0200

    Update App.py

--- App.py
+++ App.py
@@ -55,7 +55,7 @@ if st.checkbox(" **Distribution des topics :** "):
 
 
 
-top_docs_per_topic=bunka.df_top_docs_per_topic_[5]
+top_docs_per_topic=bunka.df_top_docs_per_topic
 
 if st.checkbox(" **Tops 20 vidéos par topic :** "):
     st.write(top_docs_per_topic)
